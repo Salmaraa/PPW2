017ca90b9422a03fa8ce83e83c152c2235644a6b
Salmaraa <anisahsalmarafida@mail.ugm.ac.id>
2023-11-11 11:52:23 +0700

    first commit

---
+++ resources/views/auth/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="bg-primary text-white text-center p-5 fixed-bottom d-flex align-items-center justify-content-center">
+        <p class="m-0">Created with <i class="bi bi-heart-fill text-danger"></i> by <a href="https://www.instagram.com/anisahslmrrr/" class="text-white fw-bold">Anisah Salma Rafida</a></p>
+    </footer>
